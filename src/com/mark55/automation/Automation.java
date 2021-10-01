d6f06941d656971eac90bfe0001b023aaaa2c59f
Marco <marco2002stefani@gmail.com>
2021-10-01 18:42:41 +0200

    Added new Browsers compatibility

--- src/com/mark55/automation/Automation.java
+++ src/com/mark55/automation/Automation.java
@@ -1,18 +1,39 @@
 package com.mark55.automation;
 
+import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Automation {
 	WebDriver driver;
 	
-	public Automation() {
-		WebDriverManager.chromedriver().setup();
-		driver =new ChromeDriver();
+	public Automation(String browser) {
+		switch(browser) {
+			case "firefox":WebDriverManager.firefoxdriver().setup();
+							driver =new FirefoxDriver();
+							break;
+			case "edge":WebDriverManager.edgedriver().setup();
+						driver =new EdgeDriver();
+						break;
+			case "opera":WebDriverManager.operadriver().setup();
+						driver =new OperaDriver();
+						break;
+			case "safari":WebDriverManager.safaridriver().setup();
+						driver =new SafariDriver();
+						break;
+			default:WebDriverManager.chromedriver().setup();
+					driver =new ChromeDriver();
+					break;
+		}
+		
 	}
 	
 	public void activate(String mailStr, String pwdStr) {
@@ -21,6 +42,11 @@ public class Automation {
 		WebElement img= driver.findElement(By.cssSelector("img[alt=\"Logo SSO Unipd\"]"));
 		img.click();
 		
+		try { //FireFox is slow, so it s better to wait a second
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		WebElement mail= driver.findElement(By.id("j_username_js"));
 		WebElement password= driver.findElement(By.id("password")); 
 		WebElement radio= driver.findElement(By.id("radio2")); 	
--- src/com/mark55/automation/Options.java
+++ src/com/mark55/automation/Options.java
@@ -7,9 +7,13 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JRadioButton;
 
 public class Options extends JPanel{
 	
@@ -20,7 +24,13 @@ public class Options extends JPanel{
 	private JLabel lblPwd;
 	private JLabel lblMail;
 	private JButton button;
-	
+	private JRadioButton radioFirefox;
+	private JRadioButton radioChrome;
+	private JRadioButton radioEdge;
+	private JRadioButton radioSafari;
+	private JRadioButton radioOpera;
+	private ButtonGroup radioBrowser;
+			
 	public Options() {
 		setLayout(null);
 		
@@ -37,7 +47,7 @@ public class Options extends JPanel{
 		
 		pwdField = new JTextField();
 		pwdField.setColumns(10);
-		pwdField.setBounds(158, 120, 111, 25);
+		pwdField.setBounds(158, 90, 111, 25);
 		if(pwdEnv!=null) pwdField.setText(pwdEnv);
 		add(pwdField);
 		
@@ -46,7 +56,7 @@ public class Options extends JPanel{
 		add(lblMail);
 		
 		lblPwd = new JLabel("Password");
-		lblPwd.setBounds(46, 117, 81, 31);
+		lblPwd.setBounds(43, 87, 81, 31);
 		add(lblPwd);
 		
 		lblTitle = new JLabel("MOODLE AUTOMATION");
@@ -72,12 +82,44 @@ public class Options extends JPanel{
 			}else {
 				//UNIX LIKE OS				
 			}
-				
-			Automation automation = new Automation();
+			Automation automation = new Automation(radioBrowser.getSelection().getActionCommand());
 			automation.activate(mail, pwd);
 		});
-		button.setBounds(129, 171, 111, 31);
+		button.setBounds(147, 217, 111, 31);
 		add(button);
 		
+		radioBrowser = new ButtonGroup();
+		radioFirefox = new JRadioButton("Firefox");
+		radioFirefox.setBounds(175, 144, 109, 23);
+		radioFirefox.setActionCommand("firefox");
+		add(radioFirefox);
+		
+		radioChrome = new JRadioButton("Chrome");
+		radioChrome.setBounds(60, 144, 109, 23);
+		radioChrome.setSelected(true);
+		radioChrome.setActionCommand("chrome");
+		add(radioChrome);
+		
+		radioEdge = new JRadioButton("Edge");
+		radioEdge.setBounds(281, 144, 109, 23);
+		radioEdge.setActionCommand("edge");
+		add(radioEdge);
+		
+		radioOpera = new JRadioButton("Opera");
+		radioOpera.setBounds(226, 177, 109, 23);
+		radioOpera.setActionCommand("opera");
+		add(radioOpera);
+		
+		radioSafari = new JRadioButton("Safari");
+		radioSafari.setBounds(104, 177, 109, 23);
+		radioSafari.setActionCommand("safari");
+		add(radioSafari);
+		
+		radioBrowser.add(radioFirefox);
+		radioBrowser.add(radioChrome);
+		radioBrowser.add(radioSafari);
+		radioBrowser.add(radioEdge);
+		radioBrowser.add(radioOpera);
+	
 	}
 }
